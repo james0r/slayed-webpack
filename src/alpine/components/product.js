dff95f99d22edd0f7025ddd27b7999dcd679dd78
James <james.auble@gmail.com>
2022-05-03 13:52:14 -0700

    made variant selectors fully accessible

--- src/alpine/components/product.js
+++ src/alpine/components/product.js
@@ -8,7 +8,7 @@ export default {
       selectedOption1: null,
       selectedOption2: null,
       selectedOption3: null,
-      addToCartButtonText: 'Add to cart', 
+      addToCartButtonText: 'Add to cart',
       showBuyNow: true,
       variants: [],
       init(variants, selectedVariantId) {
@@ -48,33 +48,34 @@ export default {
 
         // Set selected variant price
         this.selectedVariantPrice = variantSelected.price
-        
       },
-      onOptionChangeHandler(el) {
+      onOptionChangeHandler(el, option) {
         this.quantity = 1
-        let variantExists = false 
-        
+        let variantExists = false
+
         this.variants.forEach((variant) => {
-          if (variant.option1 == this.selectedOption1 && variant.option2 == this.selectedOption2 && variant.option3 == this.selectedOption3) {
+          if (
+            variant.option1 == this.selectedOption1 &&
+            variant.option2 == this.selectedOption2 &&
+            variant.option3 == this.selectedOption3
+          ) {
             this.selectedVariant = variant.id
             this.selectedVariantPrice = variant.price
             variantExists = true
-            
-            window.history.replaceState({ }, '', `${el.dataset.url}?variant=${this.selectedVariant}`);
 
-           // Set button text, button visibility, and quantity input
-           if (variant.available) {
-            this.quantity = 1
-            this.addToCartButtonText = 'Add to cart'
-            this.showBuyNow = true
-          } else {
-            this.quantity = 0
-            this.addToCartButtonText = 'Out of stock'
-            this.showBuyNow = false
-          }
+            // Set button text, button visibility, and quantity input
+            if (variant.available) {
+              this.quantity = 1
+              this.addToCartButtonText = 'Add to cart'
+              this.showBuyNow = true
+            } else {
+              this.quantity = 0
+              this.addToCartButtonText = 'Out of stock'
+              this.showBuyNow = false
+            }
 
             this.replaceSection(el)
-          } 
+          }
         })
 
         if (!variantExists) {
@@ -83,18 +84,37 @@ export default {
           this.showBuyNow = false
           this.quantity = 0
         }
+
+        window.history.replaceState(
+          {},
+          '',
+          `${el.dataset.url}?variant=${this.selectedVariant}&lastSelectedOption=${option}`
+        )
       },
       replaceSection(el) {
-        fetch(`${el.dataset.url}?variant=${this.selectedVariant}&section_id=${el.dataset.section}`)
-        .then((response) => response.text())
-        .then((responseText) => {
-          const html = new DOMParser().parseFromString(responseText, 'text/html')
-          const destination = document.querySelector('.pdp-main')
-          const source = html.querySelector('.pdp-main')
+        fetch(
+          `${el.dataset.url}?variant=${this.selectedVariant}&section_id=${el.dataset.section}`
+        )
+          .then((response) => response.text())
+          .then((responseText) => {
+            const html = new DOMParser().parseFromString(
+              responseText,
+              'text/html'
+            )
+            const destination = document.querySelector('.pdp-main')
+            const source = html.querySelector('.pdp-main')
 
-          destination.innerHTML = source.innerHTML
-        });
-      }
+            destination.innerHTML = source.innerHTML
+
+            const params = new Proxy(new URLSearchParams(window.location.search), {
+              get: (searchParams, prop) => searchParams.get(prop),
+            });
+
+            let optionId = params.lastSelectedOption
+
+            document.getElementById(optionId).focus()
+          })
+      },
     }
-  }
+  },
 }
